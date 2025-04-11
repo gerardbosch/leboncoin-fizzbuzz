636f2f913988f34adae12007a7287bd89c8e46cd
Gerard Bosch <gerard.bosch@gmail.com>
2025-04-11 16:11:42 +0200

    Initialize project

---
+++ src/test/kotlin/xyz/gerardbosch/leboncoinfizzbuzz/App.kt
@@ -0,0 +1,13 @@
+package xyz.gerardbosch.leboncoinfizzbuzz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class App {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
